f33c30451ca5dfe5f82bfaf43f3ce6e3187d02af
michaeldavidpryor <mdpcaps@gmail.com>
2020-08-29 12:03:09 -0400

    quick

--- pages/index.js
+++ pages/index.js
@@ -13,15 +13,15 @@ export default function Home({ allPostsData }) {
       </Head>
 
       <section className={utilStyles.headingMd}>
-        <p class="lead">
+        <p className="lead">
           My name is Michael and I am a classically trained chef that has taken the leap in to web
           development.{" "}
         </p>
-        <p>
+        <p className="lead">
           I have managed teams in high pressure environments and I wish to bridge these skills with
           my new ones.
         </p>
-        <p>
+        <p className="lead">
           Thank you for your time and consideration. Here you will find the start of a successful
           career in web development.
         </p>
